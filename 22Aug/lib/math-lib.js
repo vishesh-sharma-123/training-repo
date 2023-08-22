6bb1223758694b798d71bab5b3375e8e855eb970
vishesh-sharma-123 <vishesh.sharma@iiitg.ac.in>
2023-08-22 10:42:39 +0530

    22aug strt

---
+++ 22Aug/lib/math-lib.js
@@ -0,0 +1,14 @@
+export function add(a,b){
+    console.log("sum",a+b);
+}
+
+function sub(a,b){
+    console.log("sub",a-b);
+}
+function mul(a,b){
+    console.log("mul",a*b);
+}
+
+function div(a,b){
+    console.log("div",a/b);
+}
