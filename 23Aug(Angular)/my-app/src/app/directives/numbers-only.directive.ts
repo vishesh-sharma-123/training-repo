7b24aa27b0b217cb9cc54406ddb2f6bdaf218a61
vishesh-sharma-123 <vishesh.sharma@iiitg.ac.in>
2023-08-25 10:33:41 +0530

    decorator in angular

--- 23Aug(Angular)/my-app/src/app/directives/numbers-only.directive.ts
+++ 23Aug(Angular)/my-app/src/app/directives/numbers-only.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appNumbersOnly]'
@@ -6,5 +6,12 @@ import { Directive } from '@angular/core';
 export class NumbersOnlyDirective {
 
   constructor() { }
+
+  @HostListener('keypress', ['$event'])
+  restrictNumbersOnly(event: KeyboardEvent){
+    if(event.keyCode<48 || event.keyCode >57){
+      event.preventDefault();
+    }
+  }
 
 }
